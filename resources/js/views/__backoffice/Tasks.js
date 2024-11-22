d142c3da7d38a7f8f221127a27f924f16144076d
Furkan Leylek <furkanyasinn.1@outlook.com>
2024-11-22 13:44:33 +0300

    feat: implement real-time notification system with Pusher integration

--- resources/js/views/__backoffice/Tasks.js
+++ resources/js/views/__backoffice/Tasks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Master as MasterLayout } from './layouts';
 import TaskBoard from './tasks/components/taskboard';
 import TaskForm from './tasks/components/crud/taskform';
+import GetNotification from './tasks/components/get-notification';
 function Tasks(props) {
 
     return (
@@ -9,8 +10,11 @@ function Tasks(props) {
             {...props}
             pageTitle={Lang.get('navigation.tasks')}
         >
+            
             <TaskForm />
+            <GetNotification/>
             <TaskBoard />
+            
         </MasterLayout>
     );
 }
